1999d65a28304c1460cd67522ab240265763db2c
divyeshds2012 <divyeshds2012@gmail.com>
2020-05-30 23:54:08 +0530

    Extra step to remove hiddnen entries using \r

--- DNA_case_study/dna.py
+++ DNA_case_study/dna.py
@@ -4,6 +4,11 @@ f = open(inputfile, 'r')
 seq = f.read()
 print(seq)
 
-# to remove extra spliting of lines given by \n
+# to remove extra line beaks given by \n
 seq = seq.replace("\n", "")
+print(seq)
+
+# sometime we have unother character hiding in the string and than can be replced by "\r "
+#This is extra step just to be on safer side
+seq = seq.replace("\r", "")
 print(seq)
